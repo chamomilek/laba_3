b308c5d38cf1f93abe55f9cbd81531340b8bc416
Karina Osoprilko <chamomileekk@gmail.com>
2020-11-23 03:21:31 +0300

    Gorner table model hope ready

---
+++ src/bsu/rfe/java/group10/lab3/Osoprilko/varB/GornerTableCellRenderer.java
@@ -0,0 +1,15 @@
+package bsu.rfe.java.group10.lab3.Osoprilko.varB;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class GornerTableCellRenderer implements TableCellRenderer{
+    @Override
+    public Component getTableCellRendererComponent(JTable arg0, Object arg1,
+                                                   boolean arg2, boolean arg3, int arg4, int arg5) {
+// TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/bsu/rfe/java/group10/lab3/Osoprilko/varB/MainFrame.java
@@ -0,0 +1,4 @@
+package bsu.rfe.java.group10.lab3.Osoprilko.varB;
+
+public class MainFrame {
+}
